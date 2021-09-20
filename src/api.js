ec45ebe10ea1e0cb7258e97af8819bb641c98c97
Arnaud <nonokeeper@yahoo.fr>
2021-09-20 23:20:19 +0200

    Third API

---
+++ src/api.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// This will make mongoose not pluralize collection names at all, so mongoose.model('User', schema)
+// will store documents in the 'User' collection.
+mongoose.pluralize(null)
+
+const uri = 'mongodb+srv://mongodb_admin:e0wl8BtEQ50Uzcnu@dreamcluster.lt12i.mongodb.net/DreamDb?retryWrites=true&w=majority'
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) console.log('MongoDb connected with mongoose')
+    else console.log('MongoDb NOT connected, error : ' + err)
+  }
+)
+
+/*
+const customersRoutes = require('../routes/customersController')
+const collectionsRoutes = require('../routes/collectionsController')
+app.use('/customers', customersRoutes)
+app.use('/collections', collectionsRoutes)
+*/
+const cors = require('cors')
+
+// Middleware bodyParser.json() devient express.json() depuis 2021
+app.use(express.json())
+app.use(cors())
+
+const customersRoutes = require('../routes/customersController')
+const collectionsRoutes = require('../routes/collectionsController')
+const port = process.env.PORT || 3000
+
+app.use('/.netlify/functions/api/customers', customersRoutes)
+app.use('/.netlify/functions/api/collections', collectionsRoutes)
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
