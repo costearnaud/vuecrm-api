dffc91e6fb0fe7d3b3c2e688dd1c6cd8c1799294
Arnaud <nonokeeper@yahoo.fr>
2021-09-20 23:33:25 +0200

    Fourth API

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const collectionsRoutes = require('./routes/collectionsController')
 app.use('/customers', customersRoutes)
 app.use('/collections', collectionsRoutes)
 
+app.get('/', (req, res) => { res.send('Hello from Express!') })
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
