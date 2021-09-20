440876dfe06bc817dfff429f64492cbd391719f5
costearnaud <90699405+costearnaud@users.noreply.github.com>
2021-09-20 16:55:24 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,6 +3,11 @@ const serverless = require('serverless-http')
 
 const app = express()
 const mongoose = require('mongoose')
+
+// This will make mongoose not pluralize collection names at all, so mongoose.model('User', schema)
+// will store documents in the 'User' collection.
+mongoose.pluralize(null)
+
 const uri = 'mongodb+srv://mongodb_admin:e0wl8BtEQ50Uzcnu@dreamcluster.lt12i.mongodb.net/DreamDb?retryWrites=true&w=majority'
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },
@@ -19,15 +24,6 @@ app.use('/customers', customersRoutes)
 app.use('/collections', collectionsRoutes)
 */
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-// This will make mongoose not pluralize collection names at all, so mongoose.model('User', schema)
-// will store documents in the 'User' collection.
-mongoose.pluralize(null)
-
-// Enable Use and Modify at the same time
-// Mongoose v6 returns : invalid option ???
-// mongoose.set('useFindAndModify', false)
 
 // Middleware bodyParser.json() devient express.json() depuis 2021
 app.use(express.json())
